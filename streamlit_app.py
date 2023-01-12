ecb2e93944986356f4fbf4afb01926cd4625e90c
Aj <122399156+ajayku01@users.noreply.github.com>
2023-01-12 06:24:14 +0000

    Update streamlit_app.py
    stop displaying raw JSON output

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ streamlit.header('Fruityvice Fruit Advice!')
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+# streamlit.text(fruityvice_response.json())
 
 #take json resonse and normalise it to look more like table and less like code
 fruitvice_normalized = pandas.json_normalize(fruityvice_response.json())
